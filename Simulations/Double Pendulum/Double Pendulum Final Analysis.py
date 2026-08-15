5a690663c66d6f405ae9d5e80ecbbd3c768d1ca3
Aarav <athakur@dohacollege.com.qa>
2026-08-15 22:59:14 +0530

    Added DVs

--- Simulations/Double Pendulum/Double Pendulum Final Analysis.py
+++ Simulations/Double Pendulum/Double Pendulum Final Analysis.py
@@ -14,11 +14,13 @@ csv_fullpath = script_dir /"Summary"/f"Double Pendulum Mass Summary Statistics.c
 df = pd.read_csv(csv_fullpath)
 IV = "weight_mass"
 
-print(df[IV].describe())
-print(df[IV].value_counts().sort_index())
-
-plt.hist(df[IV], bins=20)
-plt.plot(df["test_id"], df[IV])
-plt.xlabel("Mass")
-plt.ylabel("Number of Simulations")
-plt.show()
+primary_dvs = ["total_energy_relative_loss",
+                  "trajectory_length",
+                  "q1_dominant_frequency",
+                  "q2_dominant_frequency",
+                  "q1_decay_rate",
+                  "q2_decay_rate",
+                  "q1_velocity_max_abs",
+                  "q2_velocity_max_abs",
+                  "q1_acceleration_max_abs",
+                  "q2_acceleration_max_abs"]
